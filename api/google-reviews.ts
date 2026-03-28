08bf56b77e023638fee714eb41d7e2cbee91febf
Lawrence Obidike <obidikelawrence@yahoo.com>
2026-03-28 23:24:20 +0000

    google reviews api

--- api/google-reviews.ts
+++ api/google-reviews.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(
-  req: VercelRequest,
+  _req: VercelRequest,
   res: VercelResponse
 ) {
   try {
@@ -9,14 +9,16 @@ export default async function handler(
     const PLACE_ID = process.env.GOOGLE_PLACE_ID;
 
     const response = await fetch(
-      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${PLACE_ID}&fields=reviews,rating,user_ratings_total&key=${API_KEY}`
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${PLACE_ID}&fields=name,rating,reviews,user_ratings_total&key=${API_KEY}`
     );
 
     const data = await response.json();
 
-    res.status(200).json(data.result);
+    console.log("Google API response:", data);
+
+    res.status(200).json(data);
   } catch (error) {
+    console.log("Error:", error);
     res.status(500).json({ error: "Failed to fetch reviews" });
-    console.log(error)
   }
 }
